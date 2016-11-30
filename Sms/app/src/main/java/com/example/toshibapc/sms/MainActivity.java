6067f8498d99cb5777657102667e0a35e85148da
dininta <dinintaannisa@yahoo.com>
2016-11-30 14:53:32 +0700

    Inbox

--- Sms/app/src/main/java/com/example/toshibapc/sms/MainActivity.java
+++ Sms/app/src/main/java/com/example/toshibapc/sms/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.toshibapc.sms;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -35,7 +43,9 @@ public class MainActivity extends Activity {
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                sendSMSMessage();
+                //sendSMSMessage();
+                Intent intent = new Intent(MainActivity.this, ReceiveSms.class);
+                startActivity(intent);
             }
         });
     }
