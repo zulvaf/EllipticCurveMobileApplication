a13d92502e6c8ad11c29cc42e6613176490168bb
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-12-01 00:04:25 +0700

    create compose activity

--- SMSManager/app/src/main/java/com/example/toshibapc/sms/SendSMS.java
+++ SMSManager/app/src/main/java/com/example/toshibapc/sms/SendSMS.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -30,28 +35,26 @@ public class SendSMS extends Activity {
     String phoneNo;
     String message;
 
-    //public static SendSMS instance() {
-    //    return inst;
-    //}
-
-    //@Override
-    //public void onStart() {
-    //    super.onStart();
-    //    inst = this;
-    //}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_sms);
 
+        Button btn = (Button) findViewById(R.id.btnSendSMS);
+        btn.setTextColor(Color.WHITE);
+
         sendBtn = (Button) findViewById(R.id.btnSendSMS);
-        txtPhoneNo = (EditText) findViewById(R.id.editText);
-        txtMessage = (EditText) findViewById(R.id.editText2);
+        txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                //sendSMSMessage();
+                sendSMSMessage();
                 Intent intent = new Intent(SendSMS.this, ReceiveSms.class);
                 startActivity(intent);
             }
