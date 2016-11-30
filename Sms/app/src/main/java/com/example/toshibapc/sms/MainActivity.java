6606a4c52660f0829416cd0be23b086f378f55d6
dininta <dinintaannisa@yahoo.com>
2016-11-30 11:59:19 +0700

    Initialize

---
+++ Sms/app/src/main/java/com/example/toshibapc/sms/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.toshibapc.sms;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    Button sendBtn;
+    EditText txtPhoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        txtPhoneNo = (EditText) findViewById(R.id.editText);
+        txtMessage = (EditText) findViewById(R.id.editText2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+    }
+
+    protected void sendSMSMessage() {
+        phoneNo = txtPhoneNo.getText().toString();
+        message = txtMessage.getText().toString();
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNo, null, message, null, null);
+        Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+    }
+}
